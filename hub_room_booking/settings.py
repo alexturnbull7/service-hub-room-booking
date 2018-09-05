8c33a5d881a39cc0b8d4189ffd476c610968b8bd
Alex Turnbull <alexturnbull@Alexs-MacBook-Pro.local>
2018-09-05 16:53:59 +0100

    updatd

--- hub_room_booking/settings.py
+++ hub_room_booking/settings.py
@@ -153,6 +153,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
